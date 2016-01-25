701f2a8d4305db2dda5018c570e79c7219085fb9
morgan-ashley <morgan.angela.ashley@gmail.com>
2016-01-25 15:28:04 -0800

    update

--- js/helper.js
+++ js/helper.js
@@ -1,5 +1,5 @@
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr/>';
+var HTMLheaderRole = '<span>%data%</span>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
@@ -9,7 +9,7 @@ var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
-var HTMLbioPic = '<img src="%data%" class="biopic">';
+/* var HTMLbioPic = '<img src="%data%" class="biopic">'; */
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
 var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
@@ -37,9 +37,8 @@ var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineTitle = '<a href="#">%data%';
-
+var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,14 +4,14 @@
             "name": "Las Positas College",
             "location": "Livermore, CA",
             "degree": "Associate of Arts",
-            "major": "being awesome",
+            "major": "Fine Art",
             "dates": 2008,
         },
         {
-            "name": "DublinBeautyCollege",
+            "name": "Dublin Beauty College",
             "location": "Dublin, CA",
-            "degree": "CaliforniaStateBoardLicensedCosmetologist",
-            "major": ["Cosemtology","Esthetician" ],
+            "degree": "California StateBoard Licensed Cosmetologist",
+            "major": ["Cosemtology"],
             "dates" : 2010
         }
    		],
@@ -41,65 +41,48 @@
             "url": "http: //www.udacity.com/"
         }
     ]
-}
+};
+
  education.display = function() {
 	$("#education").append(HTMLschoolStart);
-	for(var schools in education.schools) {
-		var formattedName = HTMLschoolName.replace("%data%", education.schools[schools].name);
+        for(var schools in education.schools) {
+        var formattedName = HTMLschoolName.replace("%data%", education.schools[schools].name);
         $('.education-entry:last').append(formattedName);
 
-		var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[schools].degree);
+        var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[schools].degree);
 		var formattedDates = HTMLschoolDates.replace("%data%", education.schools[schools].dates);
         $('.education-entry:last').append(formattedDates);
 
-		var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[schools].location);
+     	var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[schools].location);
        
-		var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[schools].major);
+	    var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[schools].major);
         $('.education-entry:last').append(formattedMajor);
 	};
 
-     $('#education').append(HTMLonlineClasses);
-     $("#education").append(HTMLschoolStart);
-	for(var online in education.onlineCourses) {
-        var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[online].school);
-		var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[online].title);
-        var schoolTitle = formattedOnlineSchool + formattedOnlineTitle;
-        $('.education-entry:last').append(schoolTitle);
+        $('#education').append(HTMLonlineClasses);
+        $("#education").append(HTMLschoolStart);
+	    for(var online in education.onlineCourses) {
         
-		 var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[online].dates);
-         $('.education-entry:last').append(formattedOnlineDates);
-		// var formattedURL = HTMLonlineURL.replace("%data%". education.onlineCourses[index].url);
+		var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[online].title);
+        var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[online].school);
+        var schoolTitle = formattedTitle + formattedSchool;
+        $('.education-entry:last').append(schoolTitle);
+    
+    	var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[online].dates);
+        $('.education-entry:last').append(formattedOnlineDates);
 	};
 } 
 
 education.display(); 
 
-
  var work = {
     "jobs": [
         {
-            "employer": "Hard Water",
-            "title": "Server",
-            "location": "San Francisco, CA",
-            "dates": ["2014-2015"],
-            "description": "Owned by The Slanted Door Group. Hard Water has an impressive collection of over 400 American whiskies. This not quite bar-not-quite a restaurant setting offers New Orleans cuisine with a California twist. ",
-            "display": "function"
-        },
-        {
-            "employer": "Hornblower Cruises",
-            "title": "Bartender",
-            "location": "San Francisco, CA",
-            "dates": 2015,
-            "description": "boats!",
-            "display": "function"
-        },
-        {
             "employer": "Blink Tag Inc",
-            "title": "Front-end Dev",
+            "title": "Front-end Developer",
             "location": "San Francisco, CA",
             "dates": ["2011-2014"],
             "description": "Being a contractor, I was assigned just about everything, ranging from front-end development, to photographing multi-million dollar condos across from AT&T Park. Projects included following the latest transportation trends for 511CC, managing Facebook, Pinterest, and Twitter accounts for various companies. I also wrote FourSquare tips for SFBART, and edited the homepage heroes for BART(www.bart.gov). ",
-            "display": "function"
         }
     ]
 }
@@ -122,36 +105,20 @@ education.display();
 			$(".work-entry:last").append(formattedLocation);
 
 			var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-			$(".work-entry:last").append(formattedDescription );
-			
+			$(".work-entry:last").append(formattedDescription );	
 		}
-
 	}; 
 
  var projects = {
     "projects": [
         {
             
-            "title": "Server",
-            "dates": ["2014-2015"],
-            "description": "Project description goes here",
-            "images" : ["url", "url", "url"],
+            "title": "NATCO",
+            "dates": ["2013-2014"],
+            "description": "The National Association of City Transportation Officials (NACTO) is a non-profit association that represents large cities on transportation issues of local, regional and national significance. NACTO is committed to raising the state of the practice for street design and transportation by building a common vision, sharing data, peer-to-peer exchange in workshops and conferences, and regular communication among member cities",
+            "images" : ["images/natco.jpg", "images/subsections.jpg"],
             "display": "function"
-        },
-        {
-            "title": "Server",
-            "dates": ["2014-2015"],
-            "description": "Project description goes here",
-            "images" : ["url", "url", "url"],
-            "display": "function"
-        },
-        {
-            "title": "Server",
-            "dates": ["2014-2015"],
-            "description": "Project description goes here",
-            "images" : ["url", "url", "url"],
-            "display": "function"
-        }
+        } 
     ]
 }
 
@@ -168,77 +135,85 @@ education.display();
    			$(".project-entry:last").append(formattedDescription);
 
 
-   			/* if(projects.projects[project].image.length > 0) {
-   				for (image in projects.projects[project].images) { 
-   			var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images);
-   			$(".project-entry:last").append(formattedImage);
-   			
-   			}
-   		} */
+   			if(projects.projects[project].images.length > 0) {
+   	
+   			  var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[0]);
+   			  $(".project-entry:last").append(formattedImage);
+              var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[1]);
+              $(".project-entry:last").append(formattedImage);
+       	}
    	}
 }; 
 
 var bio = {
-	"name" : "Morgan Ashley",
-	"role" : "Front-End Developer",
-	"contacts" : {
-		"email" : "morgan.angela.ashley@gmail.com",
-		"mobile" : "925-699-6868",
-		"github" : "github.com/morgan-ashley",
-		"twitter" : "@ms_morganashley",
-		"location" : "San Francisco, CA"
-	},
-	"welcomeMessage" : "I love to program!",
-	"skills" : [
-		"HTML", "CSS", "JavaScript"
-	],
-	"bioPic" : "images/morgan.jpg"
-	}
+    "name" : "Morgan Ashley",
+    "role" : "Front-End Developer",
+    "contacts" : {
+        "email" : "morgan.angela.ashley@gmail.com",
+        "mobile" : "415-555-5555",
+        "github" : "github.com/morgan-ashley",
+        "twitter" : "@ms_morganashley",
+        "location" : "San Francisco, CA"
+    },
+    "welcomeMessage" : "I love to program!",
+    "skills" : [
+        "HTML", "CSS", "Responsive Design", "Bootstrap", "JavaScript"
+    ],
+    "bioPic" : "images/morgan.jpg"
+    }
 
 bio.display = function() {
 
     var formattedName = HTMLheaderName.replace("%data%", bio.name);
     var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-    var formattedPic = HTMLbioPic.replace("%data%", bio.bioPic);
-    var formattedMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-
+    /* var formattedPic = HTMLbioPic.replace("%data%", bio.bioPic); 
+    var formattedMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage); */
        
-// for (var contacts = 0; bio.contacts.length > contacts; contact++) {
-   //  $('#footerContacts').append(HTMLcontactGeneric);
-       // for (contacts in bio.contacts){ 
             var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-                $('#footerContacts').append(formattedEmail);
-            var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-                $('#footerContacts').append(formattedMobile);
-            var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
-                $('#footerContacts').append(formattedGitHub);
-            var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-                $('#footerContacts').append(formattedTwitter);
-            var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-                $('#topContacts').append(formattedLocation);
-       
-$("#header").prepend(formattedMessage);
-$("#header").prepend(formattedPic);
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+            $('#footerContacts').append(formattedEmail);
+            $('#topContacts').append(formattedEmail)
 
- } 
+            var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+            $('#footerContacts').append(formattedMobile);                
+            $('#topContacts').append(formattedMobile)
+
+            var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
+            $('#footerContacts').append(formattedGitHub);
+            $('#topContacts').append(formattedGitHub)
+
+            var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+            $('#footerContacts').append(formattedTwitter);
+            $('#topContacts').append(formattedTwitter)
+
+            var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+            $('#footerContacts').append(formattedLocation);
+            $('#topContacts').append(formattedLocation)
+
+            if (bio.skills.length > 0) {
+            $("#header").append(HTMLskillsStart);
+            var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+            $("#skills").append(formattedSkill);
+            var formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+            $("#skills").append(formattedSkill);
+            var formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+            $("#skills").append(formattedSkill);
+            var formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+            $("#skills").append(formattedSkill);
+            var formattedSkill = HTMLskills.replace("%data%", bio.skills[4]);
+            $("#skills").append(formattedSkill);
+    }
+      
+    /* $("#header").prepend(formattedMessage);
+    $("#header").prepend(formattedPic); */
+    $("#header").prepend(formattedRole);
+    $("#header").prepend(formattedName);
+
+} 
 
 bio.display();
 work.display(); 
 projects.display();
 
-if (bio.skills.length > 0) {
-	$("#header").append(HTMLskillsStart);
-
-	var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-		$("#skills").append(formattedSkill);
-	var formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-		$("#skills").append(formattedSkill);
-	var formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-		$("#skills").append(formattedSkill);
-}
-
-$("#main").append(internationalizeButton);
+/* $("#main").append(internationalizeButton); */
 $("#mapDiv").append(googleMap);
 
